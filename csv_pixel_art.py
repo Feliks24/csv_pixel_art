cd2847976c96fa908db0c67655a39ef32f5125e9
Feliks24 <75443314+Feliks24@users.noreply.github.com>
2025-03-01 23:39:04 +0100

    Restructured
    restructured so that formating images can be added later

--- csv_pixel_art.py
+++ csv_pixel_art.py
@@ -9,71 +9,84 @@ import os
 #our file
 from settings import *
 
-
-
 def hex_to_rgb(hex_color):
     """Converts a hexadecimal color code to RGB (decimal)."""
     hex_color = hex_color.lstrip('#')
     return [int(hex_color[i:i+2], 16) for i in (0, 2, 4)]
 
 def string_to_rgb(rgb_string):
+    #perhaps dangerous, TODO: make sure its good
     return ast.literal_eval(rgb_string)
 
 def ansi(rgb_array):
     #ansi escape character for setting the color
     return f'\\e[48;2;{rgb_array[0]};{rgb_array[1]};{rgb_array[2]}m '
 
-def csv_format(inputfile, outputfile):
+def csv_format(inputfile):
     """does all the work"""
+    data = []
     with open(inputfile, newline='') as csvfile:
 
         reader = csv.reader(csvfile, delimiter=delimiter, quotechar=quotechar)
-
-        #find unique colors
+        #find unique colors and get matrix
         colors = set()
         for row in reader:
             colors.update(row)
-        define_colors = dict()
-        colorcode_RGB = (0,0,0)
+            data.append(row)
+    matrix = np.array(data,dtype='<U25')
 
-        #define the unique colors and convert to readable colorcode
+    colorcode_RGB = (0,0,0)
+    define_colors = {}
 
-        print(f'define colorcodes for colors either in:\n hex #000000 - #FFFFFF\n rgb [0,0,0] - [255,255,255]\n transparent: \'transparent\' or -1')
-        for color in colors:
-            print(f'define {color}:')
-            colorcode = input()
-            if colorcode[:1] == '!':
-                #only for testing
-                colorcode_RGB = colorcode
-            elif colorcode == 'transparent' or colorcode == '-1':
-                colorcode_RGB ='\\e[0m '
-            elif colorcode[:1] == '#':
-                colorcode_RGB = ansi(hex_to_rgb(colorcode))
-            elif colorcode[:1] == '[':
-                colorcode_RGB = ansi(string_to_rgb(colorcode))
-            else:
-                print('colorcode not recognized')
-                return
-            define_colors[color] = ratio_multiplier*colorcode_RGB
+    #define the unique colors and convert to readable colorcode
+    print(f'define colorcodes for colors either in:\n hex #000000 - #FFFFFF\n rgb [0,0,0] - [255,255,255]\n transparent: \'transparent\' or -1')
+    for color in colors:
+        print(f'define {color}:')
+            #if str(color) == '-1':
+                #colorcode_RGB ='\\e[0m '
+            #else:
+                #colorcode_RGB = ansi(string_to_rgb(color))
+        colorcode = input()
+        if colorcode[:1] == '!':
+            #only for testing, to be removed later
+            colorcode_RGB = colorcode
+        elif colorcode == "":
+            #special case, to be removed later
+            colorcode_RGB = ansi(string_to_rgb(color))
+        elif colorcode == 'transparent' or colorcode == '-1':
+            colorcode_RGB ='\\e[0m '
+        elif colorcode[:1] == '#':
+            colorcode_RGB = ansi(hex_to_rgb(colorcode))
+        elif colorcode[:1] == '[':
+            colorcode_RGB = ansi(string_to_rgb(colorcode))
+        else:
+            print('colorcode not recognized')
+            return
+        define_colors[color] = colorcode_RGB
+            
+    for w in range(matrix.shape[0]):
+        for h in range(matrix.shape[1]):
+            matrix[w,h] = define_colors[matrix[w,h]]
+    return matrix
 
-    #if file not available/overwrites previous file
+def file_to_output(inputfile, outputfile, format_function):
+    matrix = format_function(inputfile)
+    """
+        its expected that the matrix is some size
+        and has the characters already formatted
+        in the correct string now to be printed in output
+    """
     if not os.path.exists(outputfile):
         open(outputfile, 'w').close()
-    #write into new file
-    with open(inputfile, newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter=delimiter, quotechar=quotechar)
-        with open(outputfile, 'w') as writefile:
-            for row in reader:
-                writefile.write(linebegin)
-                for element in row:
-                    writefile.write(str(define_colors[element]))
-                #resets the background color
-                writefile.write('\\e[0m ')
-                #custom linened + row delimiter
-                writefile.write(lineend)
-                writefile.write(output_delimiter)
 
-
+    with open(outputfile, 'w') as writefile:
+        for row in matrix:
+            writefile.write(linebegin)
+            for element in row:
+               writefile.write(ratio_multiplier*(element))
+            #reset the terminal background + custom defined string
+            writefile.write('\\e[0m '+lineend+output_delimiter)
+    return
 
 if __name__ == "__main__":
     try:
@@ -83,7 +96,12 @@ if __name__ == "__main__":
         print("not enough arguments")
         print("$csv_pixel_art inputfile outputfile")
         sys.exit()
-    if (not inputfile[(len(inputfile)-4):] == '.csv'):
-        print('File is not csv, might cause unexpected behaviour')
     
-    csv_format(inputfile, outputfile)
+    filetype = inputfile[(len(inputfile)-4):]
+    if inputfile.endswith('.csv'):
+        file_to_output(inputfile, outputfile, csv_format)
+    elif inputfile.endswith(('.jpeg', '.png', '.jpg')):
+        print('not ready yet')
+    else:
+        print('invalid inputfile type')
+        print('make sure file is either csv, png, jpg or jpeg')
